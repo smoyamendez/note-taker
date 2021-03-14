f2e4cbe4da565656b81856d3eb10ecc6110181f5
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-03-14 15:31:12 -0400

    updated uniqueID npm due to the other one putting a /0 at the end which resulted in 404 error, everything functioning now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,7 @@
 // LOAD DATA
-const path = require('path');
 const fs = require('fs');
 const database = require('../db/db.json');
-const hyperid = require('hyperid');
+const generateUniqueId = require('generate-unique-id');
 
 
 // ROUTING
@@ -15,12 +14,18 @@ module.exports = (app) => {
     // API POST REQUESTS
     // receive a new note to save on req.body, add it to the db.json file, return new note to client, give unique id when saved (hyperid)
     app.post('/api/notes', (req, res) => {
-        const instance = hyperid();
-        const id = instance();
+        const id = generateUniqueId();
         req.body['id'] = id;
         database.push(req.body);
         fs.writeFileSync('./db/db.json', JSON.stringify(database));
         console.log(req.body);
         res.json(database);
+    })
+
+    app.delete('/api/notes/:id', (req, res) => {
+        let idIndex = database.findIndex(i => i.id === req.params.id);
+        database.splice(idIndex, 1);
+        fs.writeFileSync('./db/db.json', JSON.stringify(database));
+        res.send();
     })
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'))
     });
-    //If no matching route is found default to home
+    //If no matching route is found default to index.html
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'))
     });
