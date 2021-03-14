c34b749cdaab93d1a02a1eac732a953a58d87696
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-03-14 01:45:05 -0500

    everything functioning except Delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,15 +11,13 @@ module.exports = (app) => {
 
     // HTML GET REQUESTS
     app.get('/api/notes', (req, res) => res.json(database));
-    // app.get('/api/notes', (req, res) => {
-    //     // fs.readFile(JSON.parse(database));
-    //     // console.log(database);
-    //     // res.json(database);
-    // });
 
     // API POST REQUESTS
     // receive a new note to save on req.body, add it to the db.json file, return new note to client, give unique id when saved (hyperid)
     app.post('/api/notes', (req, res) => {
+        const instance = hyperid();
+        const id = instance();
+        req.body['id'] = id;
         database.push(req.body);
         fs.writeFileSync('./db/db.json', JSON.stringify(database));
         console.log(req.body);
